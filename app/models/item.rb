b74e832ab156490d3acc06979a332e570cd17602
ryo19861210 <superdio3633hornetkkauemi1210@gmail.com>
2022-06-28 15:22:33 +0900

    エラーメッセージの日本語化の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,11 +13,11 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name
     validates :profile
-    validates :category_genre_id, numericality: { other_than: 1, message: 'が未選択では登録できません' }
-    validates :status_genre_id, numericality: { other_than: 1, message: 'が未選択では登録できません' }
-    validates :send_price_genre_id, numericality: { other_than: 1, message: 'が未選択では登録できません' }
-    validates :place_genre_id, numericality: { other_than: 1, message: 'が未選択では登録できません' }
-    validates :scheduled_day_genre_id, numericality: { other_than: 1, message: 'が未選択では登録できません' }
+    validates :category_genre_id, numericality: { other_than: 1, message: 'は未選択では登録できません' }
+    validates :status_genre_id, numericality: { other_than: 1, message: 'は未選択では登録できません' }
+    validates :send_price_genre_id, numericality: { other_than: 1, message: 'は未選択では登録できません' }
+    validates :place_genre_id, numericality: { other_than: 1, message: 'は未選択では登録できません' }
+    validates :scheduled_day_genre_id, numericality: { other_than: 1, message: 'は未選択では登録できません' }
     validates :image
     validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, only_integer: true }
   end
