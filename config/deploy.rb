1913cc64d31f37365a599c5feb2551ab09b4c8c3
ryo19861210 <superdio3633hornetkkauemi1210@gmail.com>
2022-06-30 18:40:28 +0900

    デプロイ自動化修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.17.0"
+lock  '3.17.0'
 
 set :application, 'furima-37671'
-set :repo_url, "git@github.com:ryo19861210/furima-37671.git"
+set :repo_url, 'git@github.com:ryo19861210/furima-37671.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
